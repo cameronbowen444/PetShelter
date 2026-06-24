0ef81f50bfc3b88dec5b919bcbb94abfb54e78b4
cameronbowen444 <cameronbowen555@gmail.com>
2026-06-24 12:22:25 -0700

    testing faster fail

--- server/config/mongoose.config.js
+++ server/config/mongoose.config.js
@@ -1,8 +1,18 @@
 const mongoose = require("mongoose");
 
+const mongoURI = process.env.MONGO_URI;
+
+if (!mongoURI) {
+  console.error("MONGO_URI is missing. Check Render environment variables.");
+  process.exit(1);
+}
+
 mongoose
-  .connect(process.env.MONGO_URI)
+  .connect(mongoURI, {
+    serverSelectionTimeoutMS: 10000,
+  })
   .then(() => console.log("Established a connection to the database!"))
-  .catch((err) =>
-    console.log("Something went wrong when connecting to the database!", err)
-  );
+  .catch((err) => {
+    console.log("Something went wrong when connecting to the database!", err);
+    process.exit(1);
+  });
